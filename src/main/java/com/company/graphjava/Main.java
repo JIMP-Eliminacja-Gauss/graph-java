63a7ddaa97a779e88c20e60b4ca4b579850a1e88
Adam Smerdzyński <adamsm0002@gmail.com>
2022-05-24 18:06:10 +0200

    obsluga bledow

--- src/main/java/com/company/graphjava/Main.java
+++ src/main/java/com/company/graphjava/Main.java
@@ -1,6 +1,9 @@
 package com.company.graphjava;
 
+import com.company.graphjava.graph.Edge;
 import com.company.graphjava.graph.Graph;
+
+import java.util.Iterator;
 
 public class Main {
     private static MainWindow mainWindow;
